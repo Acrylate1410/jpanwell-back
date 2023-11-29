2b764b0711529b6a3ee1839b69ff1aad5f04d2b1
Acrylate1410 <quangnm1410@gmail.com>
2023-11-29 11:17:47 +0700

    fgjh

--- server.js
+++ server.js
@@ -1,28 +1,20 @@
 require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose');
-//const { createServer } = require('node:http');
 const routes1 = require('./routes/products');
 const routes2 = require('./routes/articles');
+const routes3 = require('./routes/messages');
 const app = express()
-//const GridFsStorage = require('multer-gridfs-storage')
-//const Grid = require('gridfs-stream')
-//const methodOverride = require('method-override')
-//app.use(methodOverride('_method'));
-//const { Server } = require('socket.io');
-//const server = createServer(app);
-//const io = new Server(server);
 const cors = (req, res, next) => {
-
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, Content-Type");
-
   next();
 };
 app.use(cors)
 app.use('/products', routes1)
 app.use('/articles', routes2)
+app.use('/messages', routes3)
 const mongoString = process.env.DATABASE_URL
 mongoose.connect(mongoString);
 const database = mongoose.connection;
